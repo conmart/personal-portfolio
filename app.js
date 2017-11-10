104c8c4109a18b63d27b1393be0c86455c37c118
conmart <connor.a.martinelli@gmail.com>
2017-11-09 17:58:24 -0800

    better styling and updated projects

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
 let myprojects = [
   {
+    title : "Crypto-Exchange",
+    link : "https://pure-headland-49327.herokuapp.com/",
+    picture : "https://steemit-production-imageproxy-upload.s3.amazonaws.com/DQmZ1oqMG55JzH59usAiynrUdamCoGJykacyZsFeHrcSfW2",
+    description : "Create an account on Crypto-Exchange to get up to date price information for the hottest cryptocurrencies, and $20,000 in fake USD to simulate investing in this new and volitile market."
+  },
+  {
+    title : "Spork",
+    link : "https://polar-tor-20830.herokuapp.com/",
+    picture : "https://storage.googleapis.com/lg_meetup_images/street-food-tracking.jpg",
+    description : "Spork is a social platform where aspiring and veteran chefs can upload, favorite, review, and search recipes. Users can also follow their favorite chefs on the platform to make sure they don't miss out on any new dishes. Bon appetit!"
+  },
+  {
     title : "Junket!",
     link : "https://vast-falls-69882.herokuapp.com/",
     picture : "http://www.travelweekly.com/uploadedImages/All_TW_Art/2016/032816/T0328COVERILLO2_HR.jpg?n=3385&width=1540&height=866&mode=crop&Anchor=MiddleCenter",
     description : "Check out Junket! to get travel tips for any upcoming destinations. Log in to share you own experiences and interact with other users."
-  },
-  {
-    title : "Race to the Iron Throne",
-    link : "https://conmart.github.io/racing-game/",
-    picture : "https://upload.wikimedia.org/wikipedia/en/d/d8/Game_of_Thrones_title_card.jpg",
-    description : "Race to be the ruler of Westeros in this GoT themed racing game"
-  },
-  {
-    title : "iToilet",
-    link : "https://pure-lake-31539.herokuapp.com/",
-    picture : "https://www.iconexperience.com/_img/o_collection_png/green_dark_grey/512x512/plain/toilet.png",
-    description : "Inspired by Seinfeld this app allows users to find usable restrooms when they're in a pinch. (Google Maps integration temporarily disabled)"
   }];
 
 
@@ -26,31 +26,28 @@ $(document).ready(function(){
   console.log('working :)');
   console.log(myprojects[0].title);
 
-  let $displayProjects = $('.display-projects');
+  $(".button-collapse").sideNav();
+
+  let $projectCards = $('.project-cards');
   let newHTML;
   myprojects.forEach((project) => {
-    newHTML = `<div class="row">
-      <div class="col m3 push-m2 center-align"><img src="${project.picture}"></div>
-
-      <div class="col m1"></div>
-
-      <div class="row">
-        <div class="col m4 push-m3">
-          <div class="card blue-grey">
-            <div class="card-content white-text">
-              <span class="card-title">${project.title}</span>
-              <p>${project.description}</p>
-            </div>
-            <div class="card-action">
-              <a href="${project.link}">Go to full project</a>
-            </div>
-          </div>
+    newHTML = `
+    <div class="col s12 m6 l6">
+      <a href="${project.link}">
+      <div class="card">
+        <div class="card-image">
+          <img src="${project.picture}">
+          <span class="card-title">${project.title}</span>
         </div>
-      </div>`;
+        <div class="card-content">
+          <p>${project.description}</p>
+        </div>
+      </div>
+    </div>`;
 
 
     console.log(newHTML);
-    $displayProjects.append(newHTML);
+    $projectCards.append(newHTML);
   });
 
 
