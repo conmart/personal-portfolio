0368330cb4d24d34fc0f5047b4ccfefa31fd70b4
conmart <connor.a.martinelli@gmail.com>
2020-07-01 15:59:01 -0700

    style project cards for desktop

--- app.js
+++ app.js
@@ -1,47 +1,72 @@
-const myprojects = [
+// const myProjects = [
+//   {
+//     title : "Crypto-Exchange",
+//     link : "https://pure-headland-49327.herokuapp.com/",
+//     picture : "https://steemit-production-imageproxy-upload.s3.amazonaws.com/DQmZ1oqMG55JzH59usAiynrUdamCoGJykacyZsFeHrcSfW2",
+//     description : "Create an account on Crypto-Exchange to get up to date price information for the hottest cryptocurrencies, and $20,000 in fake USD to simulate investing in this new and volitile market."
+//   },
+//   {
+//     title : "Spork",
+//     link : "https://polar-tor-20830.herokuapp.com/",
+//     picture : "https://storage.googleapis.com/lg_meetup_images/street-food-tracking.jpg",
+//     description : "Spork is a social platform where aspiring and veteran chefs can upload, favorite, review, and search recipes. Users can also follow their favorite chefs on the platform to make sure they don't miss out on any new dishes. Bon appetit!"
+//   },
+//   {
+//     title : "Junket!",
+//     link : "https://vast-falls-69882.herokuapp.com/",
+//     picture : "https://www.travelweekly.com/uploadedImages/All_TW_Art/2016/032816/T0328COVERILLO2_HR.jpg?n=3385&width=1540&height=866&mode=crop&Anchor=MiddleCenter",
+//     description : "Check out Junket! to get travel tips for any upcoming destinations. Log in to share you own experiences and interact with other users."
+//   }];
+
+const myProjects = [
   {
-    title : "Crypto-Exchange",
-    link : "https://pure-headland-49327.herokuapp.com/",
-    picture : "https://steemit-production-imageproxy-upload.s3.amazonaws.com/DQmZ1oqMG55JzH59usAiynrUdamCoGJykacyZsFeHrcSfW2",
-    description : "Create an account on Crypto-Exchange to get up to date price information for the hottest cryptocurrencies, and $20,000 in fake USD to simulate investing in this new and volitile market."
+    title: 'Dayta, 2020',
+    href: 'https://github.com/conmart/dayta',
+    img: 'assets/imgs/Dayta-cal.png',
+    description: 'An app for keeping track of day to day activities.',
+    tech: 'React, Firebase, Firestore',
   },
   {
-    title : "Spork",
-    link : "https://polar-tor-20830.herokuapp.com/",
-    picture : "https://storage.googleapis.com/lg_meetup_images/street-food-tracking.jpg",
-    description : "Spork is a social platform where aspiring and veteran chefs can upload, favorite, review, and search recipes. Users can also follow their favorite chefs on the platform to make sure they don't miss out on any new dishes. Bon appetit!"
+    title: 'Nim, 2020',
+    href: 'https://agitated-almeida-f6ccfb.netlify.app/',
+    img: 'assets/imgs/nim.png',
+    description: 'A mathematical game of strategy, challenge your neighbor!',
+    tech: 'React',
   },
   {
-    title : "Junket!",
-    link : "https://vast-falls-69882.herokuapp.com/",
-    picture : "https://www.travelweekly.com/uploadedImages/All_TW_Art/2016/032816/T0328COVERILLO2_HR.jpg?n=3385&width=1540&height=866&mode=crop&Anchor=MiddleCenter",
-    description : "Check out Junket! to get travel tips for any upcoming destinations. Log in to share you own experiences and interact with other users."
-  }];
-
-// $(document).ready(function(){
-//   $(".button-collapse").sideNav();
-//   const $projectCards = $('.project-cards');
-//   myprojects.forEach((project) => {
-//     const newHTML = `
-//     <div class="col s12 m6 xl3">
-//       <a href="${project.link}" target="_blank">
-//       <div class="card">
-//         <div class="card-image">
-//           <img src="${project.picture}">
-//           <span class="card-title cardTitle">${project.title}</span>
-//         </div>
-//         <div class="card-content cardContent">
-//           <p>${project.description}</p>
-//         </div>
-//       </div>
-//     </div>`;
-
-//     $projectCards.append(newHTML);
-//   });
-// });
+    title: 'Crypto-Exchange, 2017',
+    href: 'https://pure-headland-49327.herokuapp.com/',
+    img: 'assets/imgs/crypto.png',
+    description: 'C-E pulls in real time market data via CryptoCompare to simulate investing in various cryptocurrencies.',
+    tech: 'MongoDB, Express, Node.js',
+  },
+  {
+    title: 'Spork, 2017',
+    href: 'https://polar-tor-20830.herokuapp.com/',
+    img: 'assets/imgs/spork2.png',
+    description: 'A social recipe app to share your love of cooking.',
+    tech: 'Ruby on Rails',
+  },
+];
 
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('js connected')
-  var testing = 'hello there';
-  document.getElementById('projects').innerHTML = testing;
+  let newHTML = '';
+  myProjects.forEach(project => {
+    newHTML +=
+      `<a class="projectCard" href="${project.href}" target="_blank">
+        <img src="${project.img}" />
+        <div class="projectInfo">
+          <div class="projectTitle">
+            ${project.title}
+          </div>
+          <div class="description">
+            ${project.description}
+          </div>
+          <div class="tech">
+            Build with: ${project.tech}
+          </div>
+        </div>
+      </a>`;
+  })
+  document.getElementById('projectRow').innerHTML = newHTML;
 });
